6dfc580eeb5f076472c7f001250c13f1224a2d12
root <root@LAPTOP-PG22RMBN>
2023-10-10 16:50:44 -0500

    combine datasets

--- code/wrangling.py
+++ code/wrangling.py
@@ -1,3 +1,18 @@
 import os
-import csv
+import pandas as pd
+
+# Read the CSV files
+BASE_DIR = "artifacts"
+csv_path1 = os.path.join(BASE_DIR, "rawplacement.csv")
+csv_path2 = os.path.join(BASE_DIR, "Uchi PSU.csv")
+
+csv1 = pd.read_csv(csv_path1)
+csv2 = pd.read_csv(csv_path2)
+
+# Combine the two dataframes
+combined_csv = pd.concat([csv1, csv2])
+
+# Save the combined dataframe to a new CSV
+combined_csv_path = os.path.join(BASE_DIR, "rawplacement1.csv")
+combined_csv.to_csv(combined_csv_path, index=False)
 
