aa480af252e03fe98b29c6533de8db0ac72e3f91
Yalunwang <143123747+YalunWangyw26347@users.noreply.github.com>
2023-10-10 19:49:51 -0500

    clean data and set gender

--- code/wrangling.py
+++ code/wrangling.py
@@ -2,27 +2,24 @@ import os
 import csv
 import pandas as pd
 import gender_guesser.detector as gender
-INPUT_DIR = os.path.join("artifacts", "rawplacement.csv")
-OUTPUT_DIR = "artifacts"
-OUTPUT_PATH = os.path.join(OUTPUT_DIR, "cleanplacement.csv")
-# Read the CSV files
- BASE_DIR = "artifacts"
- csv_path1 = os.path.join(BASE_DIR, "rawplacement.csv")
- csv_path2 = os.path.join(BASE_DIR, "Uchi PSU.csv")
+
+### Combining data: 
  # Read the CSV files
- BASE_DIR = "artifacts"
- csv_path1 = os.path.join(BASE_DIR, "rawplacement.csv")
- csv_path2 = os.path.join(BASE_DIR, "Uchi PSU.csv")
- csv1 = pd.read_csv(csv_path1)
- csv2 = pd.read_csv(csv_path2)
- # Combine the two dataframes
- combined_csv = pd.concat([csv1, csv2])
- # Save the combined dataframe to a new CSV
- combined_csv_path = os.path.join(BASE_DIR, "rawplacement1.csv")
- combined_csv.to_csv(combined_csv_path, index=False)
+BASE_DIR = "artifacts"
+# csv_path1 = os.path.join(BASE_DIR, "rawplacement.csv")
+# csv_path2 = os.path.join(BASE_DIR, "Uchi PSU.csv")
+# csv1 = pd.read_csv(csv_path1)
+# csv2 = pd.read_csv(csv_path2)
+#  # Combine the two dataframes
+# combined_csv = pd.concat([csv1, csv2])
+#  # Save the combined dataframe to a new CSV
+# combined_csv_path = os.path.join(BASE_DIR, "rawplacement1.csv")
+# combined_csv.to_csv(combined_csv_path, index=False)
 
-
+### Clean the data, gender detector:
+INPUT_DIR = os.path.join("artifacts", "rawplacement.csv")
 def load_file():
+    d = gender.Detector()
     with open(INPUT_DIR) as f:
         reader = csv.reader(f)
         data_list=[]
@@ -35,22 +32,47 @@ def load_file():
             if "21" not in str(data[1]):
                 data[1]=2023
         for data in data_list:
-            if (("university" in data[3]) or("Uni" in data[3])or ("institu" in data[3])or("Insti" in data[3]) or ("college" in data[3])or("Coll" in data[3]) or("Prof" in data[3])or("prof" in data[3])) and (("post" not in data[3])or ("Post" not in data[3])):
+            my_list=["university","Uni","institu","Insti","college","Coll","Professor","professor"]
+            not_list=["Pos","pos"]
+            contains_keyword = any(keyword in data[3] for keyword in my_list)
+            contains_not_keyword = any(keyword in data[3] for keyword in not_list)
+            if contains_keyword ==True and contains_not_keyword== False:
                 data_clean.append(data)
     
-    d = gender.Detector()
-    for data in data_list:
-        print(data)
-        name = data[2]
-        first_name = name.split(" ")[0]
-        print(name)
-        print(first_name)
-        guess = d.get_gender(first_name)
-        print(guess)
-        data.append(guess)
-    print(data_list)
-    return
+        for data in data_clean:
+            name = data[2]
+            first_name = name.split(" ")[0]
+            guess = d.get_gender(first_name)
+            data.append(guess)
+        
+        for data in data_clean:
+            sex= data[4]
+            if "male" in sex:
+                data[4]="male"
+            if"female" in sex:
+                data[4] ="female"
+            if "female" and "male" not in sex:
+                data[4]="N/A"
+                
+        #print(len(data_clean))
+        return data_clean
 
+def write_data_to_csv(rawdata, path):
+    #print(rawdata)
+    with open(path, 'w', newline='') as csvfile:
+        fieldnames = ["School", "Year", "Name", "Placement", "Gender"]
+        writer = csv.writer(csvfile)
+        writer.writerow(fieldnames)
+        for row in rawdata:
+            writer.writerow(row)
+    return 
+if __name__ == "__main__":
+    rawdata = load_file()
+    BASE_DIR = "artifacts"
+    CSV_PATH = os.path.join(BASE_DIR, "cleaned_placement.csv")
+
+    os.makedirs(BASE_DIR, exist_ok=True)
+    write_data_to_csv(rawdata, CSV_PATH)
 
 load= load_file()
 
