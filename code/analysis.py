ec3ea0b489661b6c57960ea6e03f1df18574aee1
Vimoli <vimolimehta@gmail.com>
2023-10-11 22:04:59 -0500

    Added visualization

--- code/analysis.py
+++ code/analysis.py
@@ -34,3 +34,75 @@ calculate_distribution(male_df, "tier_distribution_male.csv")
 
 # Tier distribution for Female candidates
 calculate_distribution(female_df, "tier_distribution_female.csv")
+
+
+import plotly.graph_objects as go
+
+#Let's do the visualization of the Normal Distribution
+labels = ["Tier 1", "Tier 2", "Tier 3", "Others"]
+source_indices = [1, 2, 3, 4, 1, 2, 3, 4, 1, 2, 3, 4, 1, 2, 3, 4]
+target_indices = [1, 2, 3, 4, 1, 2, 3, 4, 1, 2, 3, 4, 1, 2, 3, 4]
+values = [15.08, 6.35, 2.38, 76.19, 3.57, 5.36, 3.57, 87.50, 2.22, 0.00, 3.33, 94.44, 0.00, 0.00, 0.00, 0.00]
+
+fig1 = go.Figure(go.Sankey(
+    node=dict(
+        pad=15,
+        thickness=20,
+        line=dict(color="black", width=0.5),
+        label=labels,
+    ),
+    link=dict(
+        source=source_indices,
+        target=target_indices,
+        value=values,
+    )
+))
+
+fig1.update_layout(title_text="School Tier vs. Placement Tier for All Candidates", font_size=10)
+fig1.show()
+
+#Let's do the visualization of the Female Candidates
+labels = ["Tier 1", "Tier 2", "Tier 3", "Others"]
+source_indices = [1, 2, 3, 4, 1, 2, 3, 4, 1, 2, 3, 4, 1, 2, 3, 4]
+target_indices = [1, 2, 3, 4, 1, 2, 3, 4, 1, 2, 3, 4, 1, 2, 3, 4]
+values = [18.18, 9.09, 9.09, 63.64, 5.56, 5.56, 0.00, 88.89, 0.00, 0.00, 3.23, 0.00, 0.00, 0.00, 0.00]
+
+fig2 = go.Figure(go.Sankey(
+    node=dict(
+        pad=15,
+        thickness=20,
+        line=dict(color="black", width=0.5),
+        label=labels,
+    ),
+    link=dict(
+        source=source_indices,
+        target=target_indices,
+        value=values,
+    )
+))
+
+fig2.update_layout(title_text="School Tier vs. Placement Tier for Male Candidates", font_size=10)
+fig2.show()
+
+#Let's do the visualization of the Female Candidates
+labels = ["Tier 1", "Tier 2", "Tier 3", "Others"]
+source_indices = [1, 2, 3, 4, 1, 2, 3, 4, 1, 2, 3, 4, 1, 2, 3, 4]
+target_indices = [1, 2, 3, 4, 1, 2, 3, 4, 1, 2, 3, 4, 1, 2, 3, 4]
+values = [30.77, 7.69, 7.69, 53.85, 0.00, 0.00, 0.00, 100.00, 0.00, 0.00, 20.00, 80.00, 0.00, 0.00, 0.00, 0.00]
+
+fig3 = go.Figure(go.Sankey(
+    node=dict(
+        pad=15,
+        thickness=20,
+        line=dict(color="purple", width=0.5),
+        label=labels,
+    ),
+    link=dict(
+        source=source_indices,
+        target=target_indices,
+        value=values,
+    )
+))
+
+fig3.update_layout(title_text="School Tier vs. Placement Tier for Female Candidates", font_size=10)
+fig3.show()
